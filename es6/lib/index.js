6bd02d081ab4b53465204107f9b90790ed7796d3
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2016-10-12 17:04:55 +0200

    Add function keep_only_fields_with_data

--- es6/lib/index.js
+++ es6/lib/index.js
@@ -16,6 +16,21 @@ function remove_all_null_fields(tweet) {
   return tweet;
 }
 
+function keep_only_fields_with_data(tweet) {
+  traverse(tweet).forEach(function (x) {
+    if ( 
+      x === null || 
+      x === undefined ||
+      x === '' ||
+      ( Array.isArray(x) && x.length === 0 ) ||
+      ( typeof x === 'object' && Object.getOwnPropertyNames(x).length === 0 )
+    ) { 
+      this.remove(); 
+    }
+  });
+  return tweet;
+}
+
 function split(tweet) {
   return tweet;
 }
@@ -23,5 +38,6 @@ function split(tweet) {
 module.exports = { 
   remove_null_fields, 
   remove_all_null_fields, 
+  keep_only_fields_with_data,
   split,
 };
---
+++ es6/test/keep_only_fields_with_data.js
@@ -0,0 +1,29 @@
+'use strict';
+const clean = require('../lib/index.js');
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const test = require('unit.js');
+
+describe("keep_only_fields_with_data", function() {
+  it('removes all properties with null values or void', function() {
+    const input = { 
+      a: 1, 
+      b: 0, 
+      c: null, 
+      d: undefined,
+      e: [], 
+      f: {},
+      g: { h: null, j: undefined, k: '' },
+      i: '',
+      l: { m: false },
+    };
+    const expected = { 
+      a: 1, 
+      b: 0, 
+      g: {},
+      l: { m: false },
+    };
+    const result = clean.keep_only_fields_with_data(input);
+    test.object(result).is(expected);
+  });
+});
