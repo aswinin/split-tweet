65203cdf7f1734e53ed2de51707ff470905baf5c
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2016-10-12 14:01:34 +0200

    Add a test to compare 2 objects

--- es6/test/null-fields.js
+++ es6/test/null-fields.js
@@ -4,8 +4,8 @@ const describe = require('mocha').describe;
 const it = require('mocha').it;
 const test = require('unit.js');
 
-describe('An object with a property sets to null', function( ) {
-  it('should remove it', function() {
+describe("An object with a null property", function() {
+  it('is returned without the null property', function() {
     const input = { 
       a: 1, 
       b: 0, 
@@ -14,20 +14,28 @@ describe('An object with a property sets to null', function( ) {
       e: [], 
       f: {}, 
     };
-    test
-      .object(input)
+    test.object(input)
         .hasLength(6)
         .hasProperties(['a', 'b', 'c', 'd', 'e', 'f']);
+    test.object(input).hasValue(null);
+    const expected = { 
+      a: 1, 
+      b: 0, 
+      d: undefined, 
+      e: [], 
+      f: {}, 
+    };
     const result = clean.remove_null_fields(input);
-    test
-      .object(result)
-        .hasLength(5)
-        .hasProperties(['a', 'b', 'd', 'e', 'f']);
+    test.object(result)
+      .notHasValue(null)
+      .hasLength(5)
+      .hasProperties(['a', 'b', 'd', 'e', 'f'])
+      .is(expected);
   });
 });
 
 describe('An object with many propertie sets to null', function( ) {
-  it('should remove them', function() {
+  it('is returned without them', function() {
     const input = { 
       a: 1, 
       b: 0, 
@@ -39,14 +47,22 @@ describe('An object with many propertie sets to null', function( ) {
       h: 23,
       i: null,
     };
-    test
-      .object(input)
+    const expected = { 
+      a: 1, 
+      b: 0, 
+      d: undefined, 
+      e: [], 
+      f: {},
+      h: 23,
+    };
+    test.object(input)
         .hasLength(9)
         .hasProperties(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']);
     const result = clean.remove_null_fields(input);
     test
       .object(result)
         .hasLength(6)
-        .hasProperties(['a', 'b', 'd', 'e', 'f', 'h']);
+        .hasProperties(['a', 'b', 'd', 'e', 'f', 'h'])
+        .is(expected);
   });
 });
